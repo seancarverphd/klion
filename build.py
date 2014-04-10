ca43fc536ab880a149a51e7121707f23143f8a60
Sean Carver <carver@american.edu>
2014-04-10 14:44:25 -0400

    Got build module working

--- build.py
+++ build.py
@@ -1,29 +1,60 @@
 import numpy
 
+# A Level is a defined (mean, std) that will be the same across certain Nodes (ie states)
 class Level(object):
-    def __init__(self,name,conductance,noise):
+    def __init__(self,name,mean,std):
         assert(isinstance(name,basestring))
-        conductance = float(conductance)
-        noise = float(noise)
+        mean = float(mean)
+        std = float(std)
         self.name = name
-        self.conductance = conductance
-        self.noise = noise
+        self.mean = mean
+        self.std = std
+    def __repr__(self):
+        return 'Level %s: (mean %s, std %s)' % (self.name,repr(self.mean),repr(self.std))
 
+# A Node is a state of the channel
 class Node(object):
     def __init__(self,name,level):
         assert(isinstance(name,basestring))
         assert(isinstance(level,Level))
         self.name = name
         self.level = level
-        
+    def __repr__(self):
+        return 'Node %s: %s' % (self.name, repr(self.level))
+
+# A Channel is a model of an ion channel
 class Channel(object):
     def __init__(self,nodes):
         for node in nodes:
             assert(isinstance(node,Node))
         self.nodes = nodes
-        self.Q = numpy.zeros(shape=(len(self.nodes),len(self.nodes)))
-        
+        self.Q = numpy.matrix(numpy.zeros(shape=(len(self.nodes),len(self.nodes))))
+    def __repr__(self):
+        s = 'Channel with Nodes:'
+        for n in self.nodes:
+            s += '\n'
+            s += repr(n)
+        return s
     def addNode(self,new):
         self.nodes.append(new)
-    
-    #def addEdge(self,one,two,q12,q21):
+    #The next two functions define the Q matrix
+    #fillQdiag() enforces (by modifying the diagonal of Q): sum of each row is zero
+    def fillQdiag(self):
+        numpy.fill_diagonal(self.Q,0.)
+        Qdiag = -self.Q.sum(axis=1)
+        numpy.fill_diagonal(self.Q,Qdiag)
+    #addEdge() modifies parameters of a transition and calls fillQdiag()
+    def addEdge(self,first,second,q12,q21):
+        self.Q[first, second] = q12
+        self.Q[second, first] = q21
+        self.fillQdiag()
+        
+#This code sets up a canonical channel
+Open = Level("Open",1.0,0.6)
+Closed = Level("Closed",0.0,0.3)
+C1 = Node("C1",Closed)
+C0 = Node("C0",Closed)
+O = Node("O",Open)
+ch3 = Channel([C1,C0,O])
+ch3.addEdge(0,1,2.,3.)
+ch3.addEdge(1,2,4.,5.)
