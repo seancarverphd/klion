345bb7740651969e12180199e20ce6b134e062d7
Sean Carver <carver@american.edu>
2014-04-19 16:16:26 -0400

    Starting to work on patch.py

--- patch.py
+++ patch.py
@@ -1,2 +1,12 @@
 import channel
+import numpy
 
+class Patch(object):
+    def __init__(self, channels):
+        self.channels = channels
+        self.assertOneChannel()
+    def assertOneChannel(self)
+        assert(len(channels) == 1)
+        assert(channels[0][0] == 1)
+        ch = channels[0][1]
+        assert(isinstance(ch,channel.Channel))
