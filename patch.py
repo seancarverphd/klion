52a9767c6aa0a56d696b0cebe4a1ec9225ea4a15
Sean Carver <carver@american.edu>
2014-04-20 18:14:24 -0400

    Programmed equilibrium routine

--- patch.py
+++ patch.py
@@ -1,12 +1,25 @@
 import channel
-import numpy
+import numpy as np
 
+def equilQ(Q):
+    (V,D) = np.linalg.eig(Q.T)   # eigenspace
+    imin = np.argmin(np.absolute(V))  # index of 0 eigenvalue
+    eigvect0 = D[:,imin]  # corresponding eigenvector
+    return eigvect0.T/sum(eigvect0) # normalize and return (fixes sign)
 class Patch(object):
     def __init__(self, channels):
         self.channels = channels
         self.assertOneChannel()
-    def assertOneChannel(self)
-        assert(len(channels) == 1)
-        assert(channels[0][0] == 1)
-        ch = channels[0][1]
-        assert(isinstance(ch,channel.Channel))
+        self.Q = self.ch.makeQ()
+        self.Mean = self.ch.makeMean()
+        self.Std = self.ch.makeStd()
+    def assertOneChannel(self):
+        assert(len(self.channels) == 1)
+        assert(self.channels[0][0] == 1)
+        self.ch = self.channels[0][1]
+        assert(isinstance(self.ch,channel.Channel))
+    def equilibrium(self):
+        self.Q = self.ch.makeQ()
+        return equilQ(self.Q)
+
+P = Patch([(1, channel.khh)])
