d5c43887a543c7cd09a30a557b8fcbb1c5e2818b
Sean Carver <carver@american.edu>
2014-06-12 12:21:20 -0400

    renamed Patch class to singleChannelPatch & cleaned up

--- patch.py
+++ patch.py
@@ -64,23 +64,16 @@ class StepProtocol(object):
                 time = self.simDataT[-1] + mag_dt
                 self.appendTrajectory(nextState,time,volts)
 
-class Patch(object):
-    def __init__(self, channels):
-        self.channels = channels
-        self.assertOneChannel()  # temporary; defines self.ch as temp handle to the channel
+class singleChannelPatch(object):
+    def __init__(self, ch):
+        self.ch = ch
         self.voltages = [channel.V0,channel.V1,channel.V2,channel.V1]  # repeat V1
         self.voltageStepDurations = [0*u.ms,default_tstop,default_tstop,default_tstop]  # default_tstop is a global parameter
         self.firstState = None # if None, draws firstState from equilibrium distribution
         self.dt = default_dt  # default_dt is a global parameter
-        # ^^^ Repeated variables affect differentiation via chain rule
         self.R = random.Random()
         self.Mean = self.ch.makeMean()
         self.Std = self.ch.makeStd()
-    def assertOneChannel(self):
-        assert(len(self.channels) == 1)
-        assert(self.channels[0][0] == 1)
-        self.ch = self.channels[0][1]
-        assert(isinstance(self.ch,channel.Channel))
     def getQ(self,volts):
         channel.VOLTAGE.remap(volts)
         return self.ch.makeQ()
@@ -103,44 +96,9 @@ class Patch(object):
             if p < rowsum:
                 return col
         assert(False)
-    def initTrajectory(self):
-        self.simStates = []
-        self.simDataT = []
-        self.simDataX = []
-        self.simDataV = []
-        if self.firstState == None: # if firstState not passed draw from equilibrium
-            state = self.select(self.equilibrium(self.voltages[0]))
-        else:
-            state = self.firstState
-        self.simStates.append(state)
-        self.simDataT.append(0.);
-        self.simDataX.append(self.R.normalvariate(self.Mean[state],self.Std[state]))
-        self.simDataV.append(parameter.m(self.voltages[0]))
-    def appendTrajectory(self,nextState,time,volts):
-        self.simStates.append(nextState)
-        self.simDataT.append(time)
-        self.simDataX.append(self.R.normalvariate(self.Mean[nextState],self.Std[nextState]))
-        self.simDataV.append(volts)
-    def sim(self,seed=None):
-        if not seed == None:  # if seed not passed, don't initialize R
-            self.R.seed(seed)
-        self.initTrajectory()
-        mag_dt = parameter.m(self.dt)
-        for i in range(len(self.voltages)):
-            volts = parameter.m(self.voltages[i])
-            nsamples = int(math.ceil(self.voltageStepDurations[i]/parameter.v(self.dt)))
-            assert(nsamples >= 0)
-            eQ = self.geteQ(self.voltages[i])
-            # The next for-loop does the simulation on states
-            for j in range(nsamples-1):
-                # self.simStates[-1] is the row of eQ to work with, selected as from equilibrium()
-                nextState = self.select(eQ,self.simStates[-1])
-                time = self.simDataT[-1] + mag_dt
-                self.appendTrajectory(nextState,time,volts)
 
-P = Patch([(1, channel.khh)])
-P.sim(seed=2)
-voltages = [channel.V0,channel.V1,channel.V2,channel.V1]  # repeat V1
+P = singleChannelPatch(channel.khh)
+voltages = [channel.V0,channel.V1,channel.V2,channel.V1]  # repeat V1; repeated variables affect differentiation via chain rule
 voltageStepDurations = [0*u.ms,default_tstop,default_tstop,default_tstop]  # default_tstop is a global parameter
 S = StepProtocol(P,voltages,voltageStepDurations)
 S.initTrajectory(2)
