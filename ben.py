ba1bda44fea2438c88d2376f78c2bf9e5ace1af4
Sean Carver <carver@american.edu>
2014-07-12 20:56:56 -0400

    Adjusting plotting in ben.py to make it work

--- ben.py
+++ ben.py
@@ -4,6 +4,7 @@ import channel
 from parameter import u
 import parameter
 import engine
+import matplotlib.pyplot as plt
 
 # Comparing time for the following two
 # functions shows that appending is
@@ -36,8 +37,9 @@ voltageStepDurations = [numpy.inf,patch.default_tstop,numpy.inf,patch.default_ts
 S = patch.StepProtocol(P,voltages,voltageStepDurations)
 FS = S.flatten(3)
 FS.sim(1)
+FS.simG(1)
 DF = FS.dataFrame()
 # RS = RepeatedSteps(P,voltages,voltageStepDurations)
 # RS.sim(rng=3,nReps=4)
-# pyplot.plot(FS.simDataT,FS.simDataX)
-# pyplot.show()
+plt.plot(DF.T_ms,DF.G_pS)
+plt.show()
