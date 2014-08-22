011c2f1dd9fb66c69215ab79963cef639e73977b
Sean Carver <carver@american.edu>
2014-08-21 20:23:22 -0400

    Wrote code for automatically generating largeKLDataSet. Also started Nalpah code

--- kulleib.py
+++ kulleib.py
@@ -19,7 +19,7 @@ def Kload(fname):
     K = Kshell(qRange,KL)
     return K
 
-class Kshell(object):
+class Kshell(object):  # better name: Kskeleton
     def __init__(self,qRange,KL):
         self.qRange = qRange
         self.KL = KL
@@ -29,6 +29,7 @@ class Kshell(object):
         plt.title('Three-State Model with Irreversible Transitions')
         plt.xlabel('First Rate Constant (kHz)')
         plt.ylabel('Second Rate Constant (kHz)')
+        # Should save nReps and derive '10^5' from nReps=100000
         cb.set_label('Kullback-Leibler Divergence to 2-State Alternative\nEach Pixel: Monte-Carlo Integral with 10^5 Samples')
         plt.show()
 
@@ -41,8 +42,8 @@ class kull(object):
         self.altParent = altParent
         self.TrueMod = trueParent.flatten()
         self.AltMod = altParent.flatten()
-        self.qRange = numpy.arange(.1,10,.1)
-        #self.qRange = numpy.arange(1,10,2)
+        self.qRange = numpy.arange(.1,10,.1) # use for plotting
+        #self.qRange = numpy.arange(1,10,2)  # use for testing
         self.nReps = 100000
     def compute(self):
         num = len(self.qRange)
@@ -58,17 +59,45 @@ class kull(object):
                 self.KL[i,j] = EfTrue + mEfAlt
                 print i, j, "up to", len(self.qRange)
     def save(self,fname):
-        f = open(fname,'wb')
-        K = pickle.dump(self,f)
-        f.close()
-    def plot(self):
-        pass 
+        Ksave(self,fname)
+
+class Nalpha(object):
+    def __init__(self,trueParent,altParent,q0=None,q1=None,q=None):
+        self.q0 = q0
+        self.q1 = q1
+        self.q = q
+        self.trueParent = trueParent
+        self.altParent = altParent
+        self.trueModel = trueParent.flatten()
+        self.altModel = altParent.flatten()
+        #self.qRange = numpy.arange(.1,10,.1) # use for plotting
+        self.qRange = numpy.arange(1,10,2) # use for testing
+        self.nReps = 100
+        self.MCSampSize = 100
+        self.alpha = 0.95
+    def compute(self):
+        num = len(self.qRange)
+        self.NA = numpy.zeros((num,num))
+        for i,q_1 in enumerate(self.qRange):
+            for j,q_0 in enumerate(self.qRange):
+                self.q0.assign(q_0)
+                self.q1.assign(q_1)
+                self.trueModel.changeModel(self.trueParent)
+                self.trueModel.sim4aic(self.nReps,self.MCSampSize)
+                # More to come ...
+    def save(self,fname):
+        NAsave(self,fname)
+        
 q0 = parameter.Parameter("q0",0.5,"kHz",log=True)
 q1 = parameter.Parameter("q1",0.25,"kHz",log=True)
 q = parameter.Parameter("q",1./6.,"kHz",log=True)
 T3 = toy.toyProtocol([q0,q1])
 T2 = toy.toyProtocol([q])
 K = kull(T3,T2,q0,q1,q)
+# Uncomment next three lines to generate 'largeKLDataSet.p' (untested); ToDo: Change code to save (& print in plot) nReps, and test
 # K.compute()
+# K.save('largeKLDataSet.p')
 # print K.KL
-  
+KP = Kload('largeKLDataSet.p')  # This file not in repository; uncomment three lines above to generate; (needs testing)
+KP.plot()
+
