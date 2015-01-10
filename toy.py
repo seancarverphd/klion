052e183332d603959098bf1ca720a49a6309e533
Sean Carver <carver@american.edu>
2015-01-10 17:23:50 -0500

    Reverting commit

--- toy.py
+++ toy.py
@@ -48,38 +48,35 @@ class RNG(object):
     def expovariate(self,q):
         return self.R.expovariate(q)
 
-class Repeated(object):
-    def __init__(self, single, rReps):
-        self.single = single
-        self.rReps = rReps
-    def sim(self,nReps=1,clear=False):
-        pass
-    def simulateOnce(self,RNG=None):
-        pass
-    def likeOnce(self):
-        pass
-        
-# Goes together: ToySimulator ToyLiker ToyExperiment ToyFlattened
-class ToySimulator(object):
-    def __init__(self, seed=None, experiment=None):
-        self.initRNG(seed)
-        if not experiment==None:
-            self.unpack(experiment)
+class flatToyProtocol(object):
+    def __init__(self, parent, seed=None):
+        self.reveal(False)  # To save hidden states, call self.reveal(True)
+        self.R = self.initRNG(seed)  # Afterwards, must call restart()
         self.restart()
-        self.reveal(False)
+        self.experiment = parent.getExperiment()
+        self.changeProtocol()
+        self.changeModel()
     def initRNG(self,seed=None): # Maybe overloaded if using a different RNG, eg rpy2
-        self.R = RNG(seed)   # RNG is a class
-    def reseed(self,seed=None):  # Clears data and resets RNG with possibly different seed
+        return RNG(seed)
+    def reseed(self,seed=None):
         self.R.setSeed(seed)
         self.restart()
     def restart(self):   # Clears data and resets RNG with same seed
-        self.R.reset()   # Resets RNG with same seed as used before
+        self.R.reset()
         self.data = []   # Data used for fitting model. (Each datum may be a tuple)
-        self.states = [] # These are the Markov states, including hidden ones.  This model is IID, though.
+        self.states = [] # These are the Markov states, including hidden ones.  This model isn't Markovian, though.
+        self.likes = []  # Likelihood (single number) of each datum. (Each datum may be a tuple) 
         self.changedSinceLastSim = False
+    def changeProtocol(self,experiment=None):
+        pass  # For this class, protocol always remains same: measure time of single event
+    def changeModel(self,experiment=None):
+        if not experiment==None:
+            self.experiment = experiment
+        self.unpack(self.experiment)
+        self.changedSinceLastSim = True ### ADD TO ENGINE!!!
+        # Don't restart(); might want to change Model and use old data 
     def unpack(self,experiment):  # For subclassing replace this code
         self.toy2, self.q, self.q0, self.q1 = experiment
-        self.changedSinceLastSim = True
     def sim(self,nReps=1,clear=False): # Only does new reps; keeps old; if (nReps < # Trajs) then does nothing
         if clear:
             self.restart()  # Resets random number generator
@@ -87,7 +84,7 @@ class ToySimulator(object):
             self.restart()
         numNewReps = nReps - len(self.data)  # Negative if decreasing nReps; if so, nReps updated data unchanged
         for n in range(numNewReps):  
-            self.data.append(self.simulateOnce(self.R)) # Don't want to use self.R elsewhere
+            self.data.append(self.simulateOnce(self.simRNG())) # Don't want to use self.R elsewhere
             if self.revealFlag:
                 self.states.append(self.recentState)
         self.nReps = nReps  # Might be decreasing nReps, but code still saves the old results
@@ -100,178 +97,27 @@ class ToySimulator(object):
             self.revealFlag = False
         return(self.revealFlag)
     def simulateOnce(self,RNG=None):  # Overload
-        R = self.getRandom(RNG)  # Pass RNG=self.R to change state of self.R; pass None for a new RNG  
+        R = self.getRandom(RNG)  # Pass RNG=self.simRNG() to change state of self.R; pass None for a new RNG  
         if self.toy2:
             self.recentState = (R.expovariate(self.q))  # Though not Markovian, we can save the hidden transition times
         else:            
             self.recentState = (R.expovariate(self.q1), R.expovariate(self.q0))
         return sum(self.recentState)
+    def simRNG(self):   # Overload if changing RNG, eg with rpy2
+        return self.R
     def getRandom(self,RNG=None):  # Overload if changing RNG eg with rpy2
         if RNG == None:   # RNG == None creates new Random Number Generator and sets seed by time
             R = random.Random()
             R.seed(long(time.time()*256))
-            return R       
         else:
-            return RNG
-
-class ToyLiker(object):
-    def __init__(self, experiment=None):
-        if not experiment==None:
-            self.unpack(experiment)
-        self.restart()
-    def restart(self):
-        self.likes = []
-        self.changedSinceLastLike = False
-    def unpack(self,experiment):  # Overload
-        self.toy2, self.q, self.q0, self.q1 = experiment
+            R = RNG
+        return R
     def likelihoods(self,passedData=None,passedLikes=None):
         if passedData == None:   # Data not passed, so ignore passed Likes (presumably not passed/None)
             data = self.data    # Use cached data
             likes = self.likes  # Append any new likes to cached self.likes
             nLast = self.nReps  # Used to restrict return value to length self.nReps in case nReps is greater than len(likes)
         elif passedLikes==None:  # Data passed, but not Likes
-            data = passedData
-            likes = []
-            nLast = len(data) # Go to end of data, don't restrict
-        else:  # both Data and Likes have been passed
-            data = passedData
-            likes = passedLikes
-            nLast = len(data)  # Go to end of data, don't restrict
-        nFirst = len(likes)
-        for datum in data[nFirst:nLast]:
-            likes.append(self.likeOnce(datum))
-        return likes[0:nLast]  # Restrict what you return to stopping point
-    def likeOnce(self,datum):  # Overload when subclassing
-        if not self.datumIntegrity(datum):
-            return -numpy.infty
-        elif self.toy2:
-            return (numpy.log(self.q) - self.q*datum)
-        elif self.q0 == self.q1:
-            return (numpy.log(self.q1) + numpy.log(self.q0) - self.q0*datum + numpy.log(datum))
-        else:
-            return (numpy.log(self.q1)+numpy.log(self.q0)+numpy.log((numpy.exp(-self.q0*datum)-numpy.exp(-self.q1*datum))/(self.q1-self.q0)))
-    def datumIntegrity(self,datum):
-        if not (isinstance(datum,float) or isinstance(datum,int)):
-            return False
-        elif datum < 0.:
-            return False
-        elif (not self.toy2) and datum == 0:
-            return False
-        else:
-            return True
-    def minuslike(self,data=None):
-        L = self.likelihoods(data)
-        return -sum(L)       
-    def like(self,data=None):
-        L = self.likelihoods(data)
-        return sum(L)
-    def pdf(self,datum):
-        return numpy.exp(self.likelihoods([datum])[0])
-    def mle(self):
-        assert(self.toy2)  # Not yet implemented for toy 3
-        return 1./numpy.mean(self.data[0:self.nReps])
-    def logf(self,data=None):
-        return numpy.matrix(self.likelihoods(data))
-    def lr(self,alt):  # likelihood ratio; self is true model
-        data = self.data[0:self.nReps]
-        return (self.logf(data) - alt.logf(data))
-    def lr_mn_sd(self,alt):  # self is true model
-        lrs = self.lr(alt)
-        mn = numpy.mean(lrs)
-        sd = numpy.std(lrs)
-        return (mn,sd)
-    def lrN(self,alt,N,M):  # add N of them, return M
-        self.sim(nReps=N*M)
-        lrNM = self.lr(alt)
-        L = numpy.reshape(lrNM,(M,N))
-        return L.sum(axis=0)
-    def aic(self,alt):  # self is true model
-        data = self.data[0:self.nReps]
-        return 2*(self.logf(data) - alt.logf(data))
-    def a_mn_sd(self,alt):  # self is true model
-        aics = self.aic(alt)
-        mn = numpy.mean(aics)
-        sd = numpy.std(aics)
-        return (mn,sd)
-    def aicN(self,alt,N,M):  # add N of them, return M
-        self.sim(nReps=N*M)
-        aicNM = self.aic(alt)
-        A = numpy.reshape(aicNM,(M,N))
-        return A.sum(axis=0)
-    def Eflogf(self, data=None):  # NEED TO ADJUST FOR REPEATED EXPERIMENTS (M and N both different from 1)
-        return(self.logf(data).mean())
-    def Eflogg(self,data):  # Must pass data, don't use self.data
-        return self.Eflogf(data)
-    
-class flatToyProtocol(object):
-    def __init__(self, parent, seed=None):
-        self.experiment = parent.getExperiment()
-        self.simulator = ToySimulator(seed,self.experiment)
-        self.liker = ToyLiker(self.experiment)
-        self.restart()
-        self.reveal(False)  # To save hidden states, call self.reveal(True)
-    def reseed(self,seed=None):  # Clears data and resets RNG with possibly different seed
-        self.simulator.reseed(seed)
-    def restart(self):   # Clears data and resets RNG with same seed
-        self.simulator.restart()
-        self.liker.restart()
-    def changeProtocol(self,experiment=None):
-        pass  # For this class, protocol always remains same: measure time of single event
-    def changeModel(self,parent):
-        self.experiment = parent.getExperiment()
-        self.simulator.unpack(self.experiment)
-        self.liker.unpack(self.experiment)
-    # redirect this function
-    # after redirecting reload liker's data, also load nReps into liker
-    def sim(self,nReps=1,clear=False): # Only does new reps; keeps old; if (nReps < # Trajs) then does nothing
-        self.simulator.sim(nReps,clear)
-        self.liker.appendNewData(self.sim.data,self.sim.nReps)
-        #if clear:
-        #    self.restart()  # Resets random number generator
-        #elif self.changedSinceLastSim:
-        #    self.restart()
-        #numNewReps = nReps - len(self.data)  # Negative if decreasing nReps; if so, nReps updated data unchanged
-        #for n in range(numNewReps):  
-        #    self.data.append(self.simulateOnce(self.simRNG())) # Don't want to use self.R elsewhere
-        #    if self.revealFlag:
-        #        self.states.append(self.recentState)
-        #self.nReps = nReps  # Might be decreasing nReps, but code still saves the old results
-        #self.changedSinceLastSim = False
-    # redirect
-    def reveal(self, flag=None):
-        return self.simulator.reveal(flag)
-        #if flag==True:
-        #    self.revealFlag = True
-        #    self.restart()  # Restart because you need to rerun to save hidden states
-        #elif flag==False:
-        #    self.revealFlag = False
-        #return(self.revealFlag)
-    ## remove
-    #def simulateOnce(self,RNG=None):  # Overload
-    #    R = self.getRandom(RNG)  # Pass RNG=self.simRNG() to change state of self.R; pass None for a new RNG  
-    #    if self.toy2:
-    #        self.recentState = (R.expovariate(self.q))  # Though not Markovian, we can save the hidden transition times
-    #    else:            
-    #        self.recentState = (R.expovariate(self.q1), R.expovariate(self.q0))
-    #    return sum(self.recentState)
-    ## remove
-    #def simRNG(self):   # Overload if changing RNG, eg with rpy2
-    #    return self.R
-    ## remove
-    #def getRandom(self,RNG=None):  # Overload if changing RNG eg with rpy2
-    #    if RNG == None:   # RNG == None creates new Random Number Generator and sets seed by time
-    #        R = random.Random()
-    #        R.seed(long(time.time()*256))
-    #    else:
-    #        R = RNG
-    #    return R
-    # Change next function so only does liker's data (& liker's nReps) or passed data; if not loaded too bad
-    def likelihoods(self,passedData=None,passedLikes=None):
-        if passedData == None:  # Data not passed, so ignore passed Likes (presumably not passed/None)
-            data = self.data    # Use cached data
-            likes = self.likes  # Append any new likes to cached self.likes
-            nLast = self.nReps  # Used to restrict return value to length self.nReps in case nReps is greater than len(likes)
-        elif passedLikes==None: # Data passed, but not Likes
             data = passedData
             likes = []
             nLast = len(data) # Go to end of data, don't restrict
