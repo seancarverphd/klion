e30d1b7d1a63fadd5478ecd659decc708ed8610b
Sean Carver <carver@american.edu>
2015-05-29 00:23:22 -0400

    Got .data attribute coded in reps.Reps()

--- kli/reps.py
+++ kli/reps.py
@@ -5,6 +5,7 @@ import toy
 
 class Reps(toy.FlatToy):
     def __init__(self, base, reps):
+        self.base = base
         self.reps = reps
         super(Reps, self).__init__(base)
 
@@ -12,20 +13,30 @@ class Reps(toy.FlatToy):
         pass
 
     def setUpExperiment(self, base):
-        self.base = base
+        pass
+        # self.base = base # moved to __init__() to avoid errors in debug
         # No changedSinceLastSim, don't want to clear base
 
     def reseed(self, seed=None):
         self.base.reseed(seed)
 
     def restart(self):
+        self.data = []
+        self.likes = []
         self.base.restart()
 
     def sim(self, nReps=1, clear=False):
         if clear:
             self.restart()
         # No changedSinceLastSim, don't want to clear base unless explicitly directed
-        self.base.sim(self.reps*nReps)  # only computes newly needed data
+        numOldReps = len(self.data)
+        numNewReps = nReps - len(self.data)
+        self.base.sim(self.reps*nReps)  # Simulate base and only compute newly needed data
+        for r in range(numNewReps):
+            datum = []
+            for d in range(self.reps):
+                datum.append(self.base.data[(numOldReps+r)*self.reps + d])
+            self.data.append(datum)
         self.nReps = nReps
 
     def debug(self, flag=None):
@@ -40,16 +51,18 @@ class Reps(toy.FlatToy):
     def likelihoods(self, passedData=None):
         if passedData is None:
             concatenatedData = None  # use data stored in self.base
+            nReps = self.base.nReps/self.reps
         else:
             concatenatedData = []
             for datum in passedData:
                 assert self.datumWellFormed(datum)
                 concatenatedData += datum  # Expected that these are lists, see datumWellFormed
+            nReps = len(concatenatedData)/self.reps
         individualLikes = self.base.likelihoods(concatenatedData)  # if
-        likereps = len(concatenatedData)/self.reps
-        arrayLikes = numpy.array(individualLikes[0:likereps*self.reps])
-        arrayLikes = numpy.reshape(arrayLikes, (likereps, self.reps))
-        return arrayLikes.sum(axis=0).tolist()
+        arrayLikes = numpy.array(individualLikes[0:nReps*self.reps])
+        arrayLikes = numpy.reshape(arrayLikes, (nReps, self.reps))
+        self.likes = arrayLikes.sum(axis=0).tolist()
+        return self.likes
 
     def likeOnce(self, datum):
         assert datumWellFormed(datum)
@@ -76,13 +89,13 @@ class Reps(toy.FlatToy):
                 datum.append(d)
             data.append(datum)
         return data
-    
+
 
     def lrN(self, alt, N, M):
         print "Don't call lrN from Reps class"
         assert False
 
-    def aicN(self, alt, N, M)
+    def aicN(self, alt, N, M):
         print "Don't call aicN from Reps class"
         assert False
 
