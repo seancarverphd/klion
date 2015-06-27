1db1461c74fc3c27ae4a3cc83f76364187912f3b
Sean Carver <carver@american.edu>
2015-06-27 01:22:41 -0400

    Repository Module Coded

--- kli/repository.py
+++ kli/repository.py
@@ -1,6 +1,33 @@
-import numpy
+class RepoField(object):
+    def __init__(self):
+        self.F = {}
+
+    def getOrMake(self, assumed, true):
+        try:
+            L = self.F[(assumed, true)]
+        except KeyError:
+            L = []
+            self.F[(assumed, true)] = L
+        return L
+
 
 class Repository(object):
     def __init__(self):
-        dataset = {}  # (basemodel,seed) : data
-        likeset = {}
+        self.likes = RepoField()
+        self.likeInfo = RepoField()
+
+class DataOnly(object):
+    def __init__(self):
+        self.data = []
+        self.mReps = 0
+        self.base = self
+    def append(self,new):
+        self.data.append(new)
+        self.mReps = len(self.data)
+    def __str__(self):
+        return str(self.data)
+    def __repr__(self):
+        return repr(self.data)
+
+
+Repo = Repository()
