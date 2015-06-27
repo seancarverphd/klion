8c826128cfb499812f6630b53b659e3dea5955ae
Sean Carver <carver@american.edu>
2015-06-27 02:17:43 -0400

    Can now pass data to DataOnly class + added sim() method to DataOnly

--- kli/repository.py
+++ kli/repository.py
@@ -17,15 +17,29 @@ class Repository(object):
         self.likeInfo = RepoField()
 
 class DataOnly(object):
-    def __init__(self):
-        self.data = []
-        self.mReps = 0
+    def __init__(self,data=None):
+        if data is None:
+            self.data = []
+        else:
+            self.data = data
+        self.mReps = len(self.data)
         self.base = self
+
+    def sim(self, mReps=None):
+        if mReps is None:
+            self.mReps = len(self.data)
+        elif mReps > len(self.data):
+            assert False
+        else:
+            self.mReps = mReps
+
     def append(self,new):
         self.data.append(new)
         self.mReps = len(self.data)
+
     def __str__(self):
         return str(self.data)
+
     def __repr__(self):
         return repr(self.data)
 
